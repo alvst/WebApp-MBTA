b906ec438243d186ba2bae714e2f7a9a4349b808
Alvie Stoddard <alviestoddard@gmail.com>
2020-04-14 00:06:13 -0400

    Alvies Code Added

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
-"""
-Simple "Hello, World" application using Flask
-"""
+from flask import Flask, render_template, request
 
-from flask import Flask
 
+import mbta_helper
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET", "POST"])
+def calculate():
+    if request.method == "POST":
+        find = request.form["location"]
+        checked = []
+        boxes = ["RapidTransit", "ExpressBus-Downtown", "LocalBus"]
+        for box in boxes:
+            if box in request.form:
+                checked.append(box)
+        # print(checked)
+        location = mbta_helper.find_stop_near(find, checked)
+        # print(location)
+        if len(location) > 1:
+            return render_template(
+                "mbta.html", location = location[0], accessible = location[1]
+            )
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+        else:
+            return render_template("mbta.html")
+    return render_template("mbta.html")
--- mbta_helper.py
+++ mbta_helper.py
@@ -1,55 +1,85 @@
-# Useful URLs (you need to add the appropriate parameters for your requests)
-MAPQUEST_BASE_URL = "http://www.mapquestapi.com/geocoding/v1/address"
-MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
+import requests
+import urllib.request
+import pprint
+import json
+import math
+def find_stop_near(nearby, check):
+    # print(":t")
+    # print(ebus, lbus, train)
+    # print(checkers)
+    info = find_location(nearby)
+    if False in info:
+        return find_stop(info, check)
+    return ["Sorry there are no stops around you 😞."]
 
-# Your API KEYS (you need to use your own keys - very long random characters)
-MAPQUEST_API_KEY = ""
-MBTA_API_KEY = ""
+def find_location(nearby):
+    key = 'sV2BLO1vtZTu2pXfQ0Wt0QBk3tM6WL1m'
+    nearby = nearby.replace(' ', '%20')
+    url = f'http://www.mapquestapi.com/geocoding/v1/address?key={key}&location={nearby},MA'
+    try:
+        with urllib.request.urlopen(url) as p:
+            data = json.load(p)
+        for locations in range(len(data["results"][0]["locations"])):
+            country = data["results"][0]["locations"][locations]["adminArea1"]
+            state = data["results"][0]["locations"][locations]["adminArea3"]
+            if country == "US" and state == "MA":
+                break
+            else:
+                if locations == len(data["results"][0]["locations"])-1:
+                    return [True]
+        latitude = data["results"][0]["locations"][locations]["latLng"]["lat"]
+        longitude = data["results"][0]["locations"][locations]["latLng"]["lng"]
+        return [False, latitude, longitude]
 
+    except Exception as e:
+        return {True, 0, 0}
 
-# A little bit of scaffolding if you want to use it
+def find_stop(info, check):
+    lat = info[1]
+    long = info[2]
+    url = f'https://api-v3.mbta.com/stops?filter%5Blatitude%5D={lat}&filter%5Blongitude%5D={long}'
+    # try:
+    with urllib.request.urlopen(url) as p:
+        data = json.load(p)
+    stop_num = 0
+    small_hyp = 10000
+    find = []
+    for stops in range(len(data["data"])):
+        ch = data["data"][stops]["relationships"]["zone"]["data"]
+        if len(check) == 1 or len(check) == 2:
+            if ch != None:
+                if "RapidTransit" in check:
+                    if data["data"][stops]["relationships"]["zone"]["data"]["id"] == "RapidTransit":
+                        find.append(stops)
+                if "LocalBus" in check:
+                    # print(data["data"][stops]["relationships"]["zone"]["data"]["id"])
+                    if data["data"][stops]["relationships"]["zone"]["data"]["id"] == "LocalBus":
+                        find.append(stops)
+                if "ExpressBus-Downtown" in check:
+                    # print(data["data"][stops]["relationships"]["zone"]["data"]["id"])
+                    if data["data"][stops]["relationships"]["zone"]["data"]["id"] == "ExpressBus-Downtown":
+                        find.append(stops)
+                    # print(find)
+            else: 
+                continue
+        else:
+            find.append(stops)
 
-def get_json(url):
-    """
-    Given a properly formatted URL for a JSON web API request, return
-    a Python JSON object containing the response to that request.
-    """
-    pass
-
-
-def get_lat_long(place_name):
-    """
-    Given a place name or address, return a (latitude, longitude) tuple
-    with the coordinates of the given place.
-    See https://developer.mapquest.com/documentation/geocoding-api/address/get/
-    for Mapquest Geocoding  API URL formatting requirements.
-    """
-    pass
-
-
-def get_nearest_station(latitude, longitude):
-    """
-    Given latitude and longitude strings, return a (station_name, wheelchair_accessible)
-    tuple for the nearest MBTA station to the given coordinates.
-    See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL
-    formatting requirements for the 'GET /stops' API.
-    """
-    pass
-
-
-def find_stop_near(place_name):
-    """
-    Given a place name or address, return the nearest MBTA stop and whether it is wheelchair accessible.
-    """
-    pass
-
-
-def main():
-    """
-    You can test all the functions here
-    """
-    pass
-
-
-if __name__ == '__main__':
-    main()
+    for stop_f in range(len(find)):
+        nearby_lat = data["data"][stop_f]["attributes"]["latitude"] - lat
+        nearby_long = data["data"][stop_f]["attributes"]["longitude"] - long
+        hyp = math.sqrt(nearby_lat **2 + nearby_long **2)
+        # print(f'{hyp} of {stop_f} vs {small_hyp} of {stop_num}')
+        if hyp < small_hyp:
+            small_hyp = hyp
+            stop_num = stop_f
+    closest_stop = data["data"][stop_num]["attributes"]["name"]
+    wheelchair = data["data"][stop_num]["attributes"]["wheelchair_boarding"]
+    if wheelchair == 0:
+        wheelchair = "No information"
+    if wheelchair == 1:
+        wheelchair = "Accessible"
+    if wheelchair == 2:
+        wheelchair = "Inaccessible"
+    print(closest_stop)
+    return [closest_stop, wheelchair]
